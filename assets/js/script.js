7b74b29067f8173323c1b1dfc3a476d7c9ba8759
isaias souza santos <isaiassouza467@gmail.com>
2022-08-06 04:24:35 -0300

    tela_login

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+
+let btn = document.getElementById('botao');
+
+
+btn.addEventListener('click', function(e){
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const senha = document.getElementById('senha').value;
+    
+    console.log(senha);
+    console.log(email);
+    
+
+
+})
